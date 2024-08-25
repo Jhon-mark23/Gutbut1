918437b8260807a3a33d35fe8e211ad49658bdf6
Jhon-mark23 <123864853+Jhon-mark23@users.noreply.github.com>
2024-08-25 15:33:13 +0800

    Add files via upload

---
+++ scripts/cmds/file.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const { GoatWrapper } = require('fca-liane-utils');
+
+module.exports = {
+	config: {
+		name: "file",
+		version: "1.0",
+		author: "Mahir Tahsan",
+		countDown: 5,
+		role: 0,
+		shortDescription: "Send bot script",
+		longDescription: "Send bot specified file ",
+		category: "𝗢𝗪𝗡𝗘𝗥",
+		guide: "{pn} file name. Ex: .{pn} filename"
+	},
+
+	onStart: async function ({ message, args, api, event }) {
+		const permission = ["61557118090040"];
+		if (!permission.includes(event.senderID)) {
+			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
+		}
+
+		const fileName = args[0];
+		if (!fileName) {
+			return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+		}
+
+		const filePath = __dirname + `/${fileName}.js`;
+		if (!fs.existsSync(filePath)) {
+			return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+		}
+
+		const fileContent = fs.readFileSync(filePath, 'utf8');
+		api.sendMessage({ body: fileContent }, event.threadID);
+	}
+};
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
