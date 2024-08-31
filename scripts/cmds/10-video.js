fa98aa0974d784bddf526df95871fe90e1841283
Jhon-mark23 <123864853+Jhon-mark23@users.noreply.github.com>
2024-08-31 10:46:48 +0800

    Update 10-video.js

--- scripts/cmds/10-video.js
+++ scripts/cmds/10-video.js
@@ -4,8 +4,15 @@ const path = require('path');
 const { getStreamFromURL, shortenURL, randomString } = global.utils;
 
 const API_KEYS = [
-    'https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyC-M5Z2_SGdMuPmYF3m5DoGHKzWGlf4gFM'
-    'AIzaSyC-M5Z2_SGdMuPmYF3m5DoGHKzWGlf4gFM',
+    ''b38444b5b7mshc6ce6bcd5c9e446p154fa1jsn7bbcfb025b3b',
+    '719775e815msh65471c929a0203bp10fe44jsndcb70c04bc42',
+    
+    'a2743acb5amsh6ac9c5c61aada87p156ebcjsnd25f1ef87037',
+    '8e938a48bdmshcf5ccdacbd62b60p1bffa7jsn23b2515c852d',
+    'f9649271b8mshae610e65f24780cp1fff43jsn808620779631',
+    '8e906ff706msh33ffb3d489a561ap108b70jsne55d8d497698',
+
+    '4bd76967f9msh2ba46c8cf871b4ep1eab38jsn19c9067a90bb',
 ];
 
 async function video(api, event, args, message) {
